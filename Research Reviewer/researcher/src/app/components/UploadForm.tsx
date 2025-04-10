4fb396cfc6700687c0b746b0d41bcaa50674cfee
HKinnovaTech <tech.hamzakhan@gmail.com>
2025-04-10 23:56:12 +0500

    width fix

--- Research Reviewer/researcher/src/app/components/UploadForm.tsx
+++ Research Reviewer/researcher/src/app/components/UploadForm.tsx
@@ -7,7 +7,7 @@ import Upload from "./chat/upload";
 export default function UploadForm() {
   return (
       <>
-        <div className="flex flex-col h-screen w-full p-12 ">
+        <div className="flex flex-col h-screen w-full gap-4 p-12 ">
         <Chatbox />  
         <Upload />
         </div>
--- Research Reviewer/researcher/src/app/components/chat/upload.tsx
+++ Research Reviewer/researcher/src/app/components/chat/upload.tsx
@@ -33,50 +33,60 @@ export default function Upload() {
 
   return (
     <>
-      <form onSubmit={handleSubmit} className="flex w-full">
-        <div className="relative">
-          <input
-            type="file"
-            accept=".pdf"
-            onChange={(e) => {
-              if (e.target.files) {
-                setFile(e.target.files[0]);
-              }
-            }}
-            className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
-          />
-          <img
-            src="/images/upload-icon.png"
-            alt="Upload"
-            className="w-10 h-10 cursor-pointer"
-          />
-        </div>
-        <div className="relative">
-          <input
-            type="file"
-            accept=".pdf"
-            onChange={(e) => {
-              if (e.target.files) {
-                setFile2(e.target.files[0]);
-              }
-            }}
-            className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
-          />
-          <img
-            src="/images/upload-icon.png"
-            alt="Upload"
-            className="w-10 h-10 cursor-pointer"
-          />
-        </div>
-        <div className="p-[1px] rounded-full bg-gradient-to-b from-[#27D1F0] from-45% to-[#FF00AA]">
-          <div className="rounded-full flex items-center bg-[#000a38] w-10 h-10">
-            <button
-              type="submit"
-              className="bg-[url('/images/upload.png')] bg-contain bg-no-repeat w-6 h-6 m-auto cursor-pointer"
-            ></button>
+      <div className="flex w-full p-2 bg-[#000B16]/70 border-1 border-[#27d2f070] rounded-4xl bg-opacity-48">
+        <input
+         type="text" 
+         value={responseText}
+         className="focus:outline-none bg-none text-white p-2 rounded-lg w-full"
+         placeholder="Response will be shown here..."
+        />
+
+        <form onSubmit={handleSubmit} className="flex w-full">
+          <div className="relative">
+            <input
+              type="file"
+              accept=".pdf"
+              onChange={(e) => {
+                if (e.target.files) {
+                  setFile(e.target.files[0]);
+                }
+              }}
+              className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
+            />
+            <img
+              src="/images/add.png"
+              alt="Upload"
+              className="w-10 h-10 cursor-pointer"
+            />
           </div>
-        </div>
-      </form>
+          <div className="relative">
+            <input
+              type="file"
+              accept=".pdf"
+              onChange={(e) => {
+                if (e.target.files) {
+                  setFile2(e.target.files[0]);
+                }
+              }}
+              className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
+            />
+            <img
+              src="/images/add.png"
+              alt="Upload"
+              className="w-10 h-10 cursor-pointer"
+            />
+          </div>
+
+          <div className="p-[1px] rounded-full bg-gradient-to-b from-[#27D1F0] from-45% to-[#FF00AA]">
+            <div className="rounded-full flex items-center bg-[#000a38] w-10 h-10">
+              <button
+                type="submit"
+                className="bg-[url('/images/upload.png')] bg-contain bg-no-repeat w-6 h-6 m-auto cursor-pointer"
+              ></button>
+            </div>
+          </div>
+        </form>
+      </div>
     </>
   );
 }
